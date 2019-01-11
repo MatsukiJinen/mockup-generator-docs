93b18eb695d9daf98ecbd2b6e789488fb2763e4e
MatsukiJinen <jinen850@gmail.com>
2019-01-11 14:56:01 +0900

    firstcommit

---
+++ script.js
@@ -0,0 +1,33 @@
+/* globals Docute */
+
+new Docute({
+  target: '#docute',
+  sourcePath: '/docs',
+  editLinkBase: 'https://github.com/MatsukiJinen/mockup-generator-docs/blob/master',
+  sidebar: [
+    {
+      links: [
+        {
+          title: 'ホーム',
+          link: '/'
+        },
+        {
+          title: 'ランダム',
+          link: '/random'
+        },
+        {
+          title: 'マニュアル',
+          link: '/manual'
+        },
+        {
+          title: '表示ブロック詳細',
+          link: '/block'
+        },
+        {
+          title: 'コンポーネントの詳細',
+          link: '/component'
+        }
+      ]
+    }
+  ]
+})
